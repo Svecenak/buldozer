3acb204002548f4aadc286226ee7361c65cd342b
Svecenak <filip.sveca@gmail.com>
2024-09-15 22:03:27 +0200

    Doopraven zbytek fcí

--- buldozer_02.py
+++ buldozer_02.py
@@ -11,42 +11,46 @@ def zadavacka(pozadavek): #požaduje nenulové číslo
 
 hp = zadavacka("hmotnost pacienta") #hmotnost pacienta jako globální proměnná (aka špinavá cesta)
 
-def davky1(dose:float) -> float: #zadávám obvyklou dávku v mg/kg a název léku
+def davky1(dose:float) -> float: #zadávám obvyklou dávku v mg/kg
     """Fce pro léky s danou dávkou na kg"""
     davka=round(dose*hp, ndigits=2)
     return davka
     
-def davky2(min, max) -> tuple: #zadávám min a max dávku v mg/kg a název léku
+def davky2(min, max) -> tuple: #zadávám min a max dávku v mg/kg
     """Fce pro léky s min. a max. dávkou na kg"""
     min_dose=round(min*hp, ndigits=2)
     max_dose=round(max*hp, ndigits=2)
     return (min_dose, max_dose)
 
-def topcap(dose, tc, lek): #zadavam davku v mg/kg, max. kg pac (vetne) a nazev leku
-    """Fce pro léky s hmotnostním omezením"""
+def topcap(dose, tc): #zadavam davku v mg/kg, max. kg pac (vetne)
+    """Fce pro léky s horním hmotnostním omezením, při překročení vrátí false"""
     if hp <= tc:
-        print(f"{lek} v dávce {round(dose*hp, ndigits=2)}mg.")
+        return round(dose*hp, ndigits=2)
     else:
-        print(f"Na {lek} je už moc velkej (do {tc}kg).")
+        return False
 
-def maxdose(dose, md, lek): #zadavam davku mg/kg, max advku v mg a nazev leku
-    """Fce pro léky s maximální dávkou"""
+def maxdose(dose, md) -> float: #zadavam davku mg/kg, max davku v mg
+    """Fce pro léky s maximální dávkou, při dosažení md vrátí její hodnotu"""
     davka=round(hp*dose, ndigits=2)
     if davka <= md:
-        print(f"{lek} v dávce {davka} mg.")
+        return davka
     else:
-        print(f"{lek} v dávce {md} mg. (maximální dávka, jinak by měl dostat {davka})")
+        return md
     
-def infuze(): #spocita bazál a rychlost za hodinu
+def infuze(inf:bool) -> float: #spocita bazál a rychlost za hodinu
+    """Při inputu 0 vrátí bazál, při inputu 1 vrátí rychlost infuze za hodinu."""
     if hp<=10:
         basal=round(hp*100,ndigits=1)
     elif hp<=20:
         basal=round(1000+(hp-10)*50, ndigits=1)
     else:
         basal=round((hp-20)*20+1500, ndigits=1)
-    print(f"Denní potřeba tekutin = {basal}ml. \ntzn. rychlost infuze ~ {round(basal/24, ndigits=1)} ml/h.")
-
     
+    if inf == 0:
+        return basal
+    else:
+        return round(basal/24, ndigits = 1)
+
 def nadpis(obsah):
     print(f"\n \n------ {obsah} ------")
     
